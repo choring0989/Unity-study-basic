236d45a0bb733e90117cdc436e321a2fe65f725f
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2020-12-14 00:00:08 +0900

    add title

---
+++ Assets/Scripts/LoaderGame.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoaderGame : MonoBehaviour
+{
+    public string SceneToLoad;
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
